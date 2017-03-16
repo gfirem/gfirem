158e76be0170a19162d9cab222b6cb7a2169f907
gfirem@gmail.com <gfirem@gmail.com>
2017-03-15 23:13:05 -0400

    Implementando los tabs para las opciones globales

--- view/html_admin.php
+++ view/html_admin.php
@@ -11,13 +11,13 @@
 ?>
 <div class="wrap">
 	
-	<?php include_once GFIREM_VIEW_PATH .'html_admin_head.php'; ?>
-	
-	<form method="post" action="options.php">
+	<?php include_once GFIREM_VIEW_PATH . 'html_admin_head.php'; ?>
+
+    <form method="post" action="options.php">
 		<?php wp_nonce_field( 'update-options' ); ?>
-		<?php settings_fields( 'gfirem_options' ); ?>
-		<?php do_settings_sections( 'gfirem_options' ); ?>
-	</form>
+		<?php settings_fields( $option ); ?>
+		<?php do_settings_sections( $option ); ?>
+    </form>
 
 </div>
 
