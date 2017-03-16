0a90481e3c711fe6cf2274855b1a29d2eb792f29
gfirem@gmail.com <gfirem@gmail.com>
2017-03-16 11:47:14 -0400

    adicionando el valor del campo establecido

--- classes/fields/signature/view/field_option.php
+++ classes/fields/signature/view/field_option.php
@@ -13,8 +13,7 @@
     <td><label><?php _e_gfirem( "Background color" ) ?></label></td>
     <td>
         <label for="label1_<?php echo esc_attr( $field['id'] ) ?>" class="howto"><?php echo esc_attr( _gfirem( "Select background color, by default White" ) ) ?></label>
-       <input type="hidden" name="backgroundcolor_<?php echo esc_attr( $field['id'] ) ?>" id="color_value" />
-    	<input type="text" name="field_options[backgroundcolor_<?php echo esc_attr( $field['id'] ) ?>]"  class="my-color-field" id="backgroundcolor_<?php echo esc_attr( $field['id'] ) ?>" >
-     
+        <input type="hidden" name="backgroundcolor_<?php echo esc_attr( $field['id'] ) ?>" id="color_value"/>
+        <input type="text" name="field_options[backgroundcolor_<?php echo esc_attr( $field['id'] ) ?>]" value="<?php echo esc_attr($field['backgroundcolor'])?>" class="my-color-field" id="backgroundcolor_<?php echo esc_attr( $field['id'] ) ?>">
     </td>
 </tr>
