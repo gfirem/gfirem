dc66683691803f86cbabd78afad8feda02399b69
gfirem@gmail.com <gfirem@gmail.com>
2017-03-19 19:06:13 -0400

    Limpiando, comentando y optimizando el codigo. Arreglando el problema de la generacion de las imagenes y su eliminacion, tambien el delay cuando se creaba o editaba.

--- classes/fields/signature/assets/js/signature.js
+++ classes/fields/signature/assets/js/signature.js
@@ -12,77 +12,47 @@ var wrappers = document.getElementsByClassName("gfirem-signature-pad");
 [].forEach.call(wrappers, process_fields);
 
 function process_fields(item, index) {
-	var field_container = item.querySelector("[data-action=store-sign]"),		
+	var field_container = item.querySelector("[data-action=store-sign]"),
 		clearButton = item.querySelector("[data-action=clear]"),
 		canvas = item.querySelector("canvas"),
-		data = field_container.getAttribute('value'),		
+		data = field_container.getAttribute('value'),
 		id = field_container.getAttribute('id'),
 		signaturePad;
 
-	// Adjust canvas coordinate space taking into account pixel ratio,
-	// to make it look crisp on mobile devices.
-	// This also causes canvas to be cleared.
-	function resizeCanvas() {
-		// When zoomed out to less than 100%, for some very strange reason,
-		// some browsers report devicePixelRatio as less than 1
-		// and only part of the canvas is cleared then.
-		var ratio = Math.max(window.devicePixelRatio || 1, 1);
-		canvas.width = canvas.offsetWidth * ratio;
-		canvas.height = canvas.offsetHeight * ratio;
-		canvas.getContext("2d").scale(ratio, ratio);
-	}
-    
-	//window.onresize = resizeCanvas;
-	//resizeCanvas();
-
 	signaturePad = new SignaturePad(canvas, {
 		onEnd: function (event) {
-			if (!gfirem_signature.is_front) {
-				 var dotCollection={};
-				 var merge = {};
-				
-				 merge['uri']=signaturePad.toDataURL("image/jpeg");				 
-				 var dataCollection = signaturePad.toData();
-				 merge['point'] =dataCollection;
-				 if (data) {		
-		         var dotCollection =JSON.parse(data);
-		         merge['point'] = dataCollection.concat(dotCollection.point);
-		         merge['id']=dotCollection.id;
-	             }
-	             else{
-	          	  merge['point'] = dataCollection;
-	          	  merge['id']="";
-	             }	  
-				field_container.setAttribute('value', JSON.stringify(merge));
-				
+			var dotCollection = {};
+			var merge = {};
+
+			merge['uri'] = signaturePad.toDataURL("image/jpeg");
+			var dataCollection = signaturePad.toData();
+			merge['point'] = dataCollection;
+			if (data) {
+				dotCollection = JSON.parse(data);
+				merge['point'] = dataCollection.concat(dotCollection.point);
+				merge['id'] = dotCollection.id;
 			}
+			else {
+				merge['point'] = dataCollection;
+			}
+			field_container.setAttribute('value', JSON.stringify(merge));
 		},
-		backgroundColor:gfirem_signature.config[id].background,
-		penColor:gfirem_signature.config[id].pencolor,
-		width:gfirem_signature.config[id].width,
-		height:gfirem_signature.config[id].height
+		backgroundColor: gfirem_signature.config[id].background,
+		penColor: gfirem_signature.config[id].pencolor,
+		width: gfirem_signature.config[id].width,
+		height: gfirem_signature.config[id].height
 	});
 
 	if (data) {
 		var pointData = JSON.parse(data);
+		console.log(pointData);
 		signaturePad.fromData(pointData.point);
 	}
 
-	if (gfirem_signature.is_front) {
-		signaturePad.off();
-	}
-	else {
-		clearButton.addEventListener("click", function (event) {
-			/*var signatureImage =signaturePad.toDataURL("image/jpeg"); 
-			var image = new Image();
-        image.src = signatureImage;
-
-        var w = window.open("");
-        w.document.write(image.outerHTML);*/
-			signaturePad.clear();
-			event.preventDefault();
-			return false;
-		});
-	}
+	clearButton.addEventListener("click", function (event) {
+		signaturePad.clear();
+		event.preventDefault();
+		return false;
+	});
 }
 
--- classes/fields/signature/signature.php
+++ classes/fields/signature/signature.php
@@ -15,6 +15,7 @@ class signature extends gfirem_field_base {
 	public $areAllSignatureFieldVisited = false;
 	public $form_id;
 	public $is_front = false;
+	private $last_saved = array();
 	
 	function __construct() {
 		parent::__construct( 'signature', _gfirem( 'Signature' ),
@@ -27,139 +28,144 @@ class signature extends gfirem_field_base {
 			),
 			_gfirem( 'Show a Signature Pad.' )
 		);
-		add_action( 'admin_footer', array( $this, 'add_script_in_front' ) );
-		add_action( 'wp_footer', array( $this, 'add_script_in_front' ) );
-		/*
-		 * Filtros a implementar para procesar las entradas y establecer la imagen que se quiere salvar. Hay que iterar sobre todos los id en item
-		 * meta para determinar cuales son del tipo correspondiente, igual se pueden obtener todos los id de este tipo y agarrar sus valores para procesarlos
-		 * despues asignarlos de nuevo. Victor esto lo ib a implementar en el padre pero no tiene mucho sentido estar procesandolo en cada campo aunque no sea necesario
-		 */
-		add_filter('frm_pre_create_entry', array( $this, 'process_pre_create_entry' ), 10, 2);
-		add_filter('frm_pre_update_entry', array( $this, 'process_pre_update_entry' ), 10, 2);
+		add_action( 'admin_footer', array( $this, 'add_script' ) );
+		add_action( 'wp_footer', array( $this, 'add_script' ) );
+		add_filter( 'frm_pre_create_entry', array( $this, 'process_pre_create_entry' ), 10, 1 );
+		add_filter( 'frm_pre_update_entry', array( $this, 'process_pre_update_entry' ), 10, 2 );
+		add_action( 'frm_before_destroy_entry', array( $this, 'process_destroy_entry' ), 10, 2 );
 	}
 	
-	public function process_pre_create_entry($values, $id){
-		$t = $values;			
-		$item_meta_collection =$values['item_meta'];	
-		foreach ($item_meta_collection as $key => $value) {
-			     $field_type =FrmField::get_type($key);
-			   	 if ($field_type=='signature') {			   	 
-			   	 	$decoded_value = json_decode($value,true);			   	  
-			   	 	$preparedData=stripslashes_deep( $decoded_value['uri'] );			   	 	
-			   	 	$data_uri = $preparedData;			   	 	
-					$encoded_image = explode(",", $data_uri)[1];
-					$decoded_image = base64_decode($encoded_image);				
-					$path=	wp_normalize_path(wp_upload_dir()['path']); 
-					$guid_image_name = $this->GUID();	
-					$filename = $path."/".$guid_image_name.".png";				
-					file_put_contents($filename, $decoded_image);
-					$filetype = wp_check_filetype( basename( $filename ), null ); 	
-					$wp_upload_dir = wp_upload_dir();
-					// Prepare an array of post data for the attachment.
-					$attachment = array(
-						'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ), 
-						'post_mime_type' => $filetype['type'],
-						'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
-						'post_content'   => '',
-						'post_status'    => 'inherit'
-					); 	
-					$attach_id = wp_insert_attachment( $attachment, $filename);	
-					// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
-					require_once( ABSPATH . 'wp-admin/includes/image.php' );
-					// Generate the metadata for the attachment, and update the database record.
-					$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
-					wp_update_attachment_metadata( $attach_id, $attach_data );
-				    $decoded_value['uri']=$attach_id;
-				    $decoded_value['id']=$attach_id;
-				    $value = json_encode($decoded_value);
-				    $values['item_meta'][$key]=$value;					
-			   	 }
-		}			
-		return $values;
-	}	
-
 	/**
-  	* Generate GUID
-  	* * @return string
-  	*/
- 	private function GUID() {
-  		if ( function_exists( 'com_create_guid' ) === true ) {
-   			return trim( com_create_guid(), '{}' );
-  			}
-  		return sprintf( '%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand( 0, 65535 ), mt_rand( 0, 65535 ), mt_rand( 0, 65535 ), mt_rand( 16384, 20479 ), mt_rand( 32768, 49151 ), mt_rand( 0, 65535 ), mt_rand( 0, 65535 ), mt_rand( 0, 65535 ) );
- 	}
-	
-	public function process_pre_update_entry($values, $id){
-		$t = $values;
-		$item_meta_collection =$values['item_meta'];	
-		foreach ($item_meta_collection as $key => $value) {
-			     $field_type =FrmField::get_type($key);
-			   	 if ($field_type=='signature') {			   	 
-			   	 	$decoded_value = json_decode($value,true);	
-			   	 	wp_delete_attachment( $decoded_value['id'] );		   	  
-			   	 	$preparedData=stripslashes_deep( $decoded_value['uri'] );			   	 	
-			   	 	$data_uri = $preparedData;			   	 	
-					$encoded_image = explode(",", $data_uri)[1];
-					$decoded_image = base64_decode($encoded_image);				
-					$path=	wp_normalize_path(wp_upload_dir()['path']); 
-					$guid_image_name = $this->GUID();	
-					$filename = $path."/".$guid_image_name.".png";				
-					file_put_contents($filename, $decoded_image);
-					$filetype = wp_check_filetype( basename( $filename ), null ); 	
-					$wp_upload_dir = wp_upload_dir();
-					// Prepare an array of post data for the attachment.
-					$attachment = array(
-						'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ), 
-						'post_mime_type' => $filetype['type'],
-						'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
-						'post_content'   => '',
-						'post_status'    => 'inherit'
-					); 	
-					$attach_id = wp_insert_attachment( $attachment, $filename);	
-					// Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
-					require_once( ABSPATH . 'wp-admin/includes/image.php' );
-					// Generate the metadata for the attachment, and update the database record.
-					$attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
-					wp_update_attachment_metadata( $attach_id, $attach_data );
-				    $decoded_value['uri']=$attach_id;
-				    $decoded_value['id']=$attach_id;
-				    $value = json_encode($decoded_value);
-				    $values['item_meta'][$key]=$value;					
-			   	 }
+	 * Destroy the attached image to the entry
+	 *
+	 * @param $id
+	 * @param $entry
+	 */
+	public function process_destroy_entry( $id, $entry ) {
+		$entry_with_meta = FrmEntry::getOne( $id, true );
+		foreach ( $entry_with_meta->metas as $key => $value ) {
+			$field_type = FrmField::get_type( $key );
+			if ( $field_type == 'signature' && ! empty( $value ) ) {
+				$decoded_value = json_decode( $value, true );
+				if ( is_array( $decoded_value ) ) {
+					if ( ! empty( $decoded_value['id'] ) ) {
+						wp_delete_attachment( $decoded_value['id'], true );
+					}
+				}
+			}
 		}
+	}
+	
+	/**
+	 * Process the entry before created it in database
+	 *
+	 * @param $values
+	 *
+	 * @return mixed
+	 */
+	public function process_pre_create_entry( $values ) {
+		$values['item_meta'] = $this->save_signature( $values['item_meta'], $values['form_id'] );
+		
 		return $values;
 	}
 	
-	public function add_script_in_front( $hook ) {
-		if ( $this->areAllSignatureFieldVisited ) {
-			$this->load_script();
-		}
+	/**
+	 * Process the entry before is updated in the database
+	 *
+	 * @param $values
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	public function process_pre_update_entry( $values, $id ) {
+		$values['item_meta'] = $this->save_signature( $values['item_meta'], $values['form_id'], true );
+		
+		return $values;
 	}
 	
-	private function load_script( $front = false ) {
-		$base_url = plugin_dir_url( __FILE__ ) . 'assets/';
-		wp_enqueue_style( 'signature_pad', $base_url . 'css/signature_pad.css', array(), $this->version );
-		wp_enqueue_style( 'dashicons' );
-		wp_enqueue_script( 'signature_pad', $base_url . 'js/signature_pad.js', array( 'jquery' ), $this->version, true );
-		wp_enqueue_script( 'gfirem_signature', $base_url . 'js/signature.js', array( "jquery" ), $this->version, true );
-		$params = array(
-			'is_front' => $this->is_front,
-		);
-		$signatureFields = FrmField::get_all_types_in_form( $this->form_id, $this->slug );
-		foreach ( $signatureFields as $key => $value ) {
-			$backgroundcolor                                  = FrmField::get_option( $value, "backgroundcolor" );
-			$pencolor                                         = FrmField::get_option( $value, "pencolor" );
-			$width                                            = FrmField::get_option( $value, "width" );
-			$height                                           = FrmField::get_option( $value, "height" );
-			$params['config'][ 'field_' . $value->field_key ] = array(
-				'background' => $backgroundcolor,
-				'pencolor'   => $pencolor,
-				'width'      => $width,
-				'height'     => $height
-			);
+	/**
+	 * Save the signature to wp attachment
+	 *
+	 * @param $fields_collections
+	 * @param $form_id
+	 * @param bool $delete_before
+	 *
+	 * @return mixed
+	 */
+	public function save_signature( $fields_collections, $form_id, $delete_before = false ) {
+		foreach ( $fields_collections as $key => $value ) {
+			$field_type = FrmField::get_type( $key );
+			if ( $field_type == 'signature' && ! empty( $value ) ) {
+				$decoded_value = json_decode( $value, true );
+				if ( is_array( $decoded_value ) ) {
+					if ( $delete_before && ! empty( $decoded_value['id'] ) ) {
+						wp_delete_attachment( $decoded_value['id'], true );
+					}
+					$prepared_data = stripslashes_deep( $decoded_value['uri'] );
+					$exploded_data = explode( ",", $prepared_data );
+					$decoded_image = base64_decode( $exploded_data[1] );
+					$upload_dir    = wp_upload_dir();
+					$file_id       = $this->slug . '_' . $form_id . '_' . $key . '_' . time();
+					$file_name     = $file_id . ".png";
+					$full_path     = wp_normalize_path( $upload_dir['path'] . DIRECTORY_SEPARATOR . $file_name );
+					$upload_file   = wp_upload_bits( $file_name, null, $decoded_image );
+					if ( ! $upload_file['error'] ) {
+						$wp_filetype   = wp_check_filetype( $file_name, null );
+						$attachment    = array(
+							'post_mime_type' => $wp_filetype['type'],
+							'post_title'     => preg_replace( '/\.[^.]+$/', '', $file_name ),
+							'post_content'   => '',
+							'post_status'    => 'inherit'
+						);
+						$attachment_id = wp_insert_attachment( $attachment, $upload_file['file'] );
+						if ( ! is_wp_error( $attachment_id ) ) {
+							require_once( ABSPATH . "wp-admin" . '/includes/image.php' );
+							$attachment_data = wp_generate_attachment_metadata( $attachment_id, $upload_file['file'] );
+							wp_update_attachment_metadata( $attachment_id, $attachment_data );
+							$decoded_value['uri']          = $attachment_id;
+							$decoded_value['id']           = $attachment_id;
+							$value                         = json_encode( $decoded_value );
+							$fields_collections[ $key ]    = $value;
+							$_POST['item_meta'][ $key ]    = $value;//Used to update the current request
+							$_REQUEST['item_meta'][ $key ] = $value;//Used to update the current request
+							$this->last_saved[ $key ]      = $value;
+						}
+					}
+				}
+			}
 		}
-		wp_localize_script( 'gfirem_signature', 'gfirem_signature', $params );
 		
+		return $fields_collections;
+	}
+	
+	/**
+	 * Add script needed to load the field
+	 *
+	 * @param $hook
+	 */
+	public function add_script( $hook ) {
+		if ( $this->areAllSignatureFieldVisited ) {
+			$base_url = plugin_dir_url( __FILE__ ) . 'assets/';
+			wp_enqueue_style( 'signature_pad', $base_url . 'css/signature_pad.css', array(), $this->version );
+			wp_enqueue_style( 'dashicons' );
+			wp_enqueue_script( 'signature_pad', $base_url . 'js/signature_pad.js', array( 'jquery' ), $this->version, true );
+			wp_enqueue_script( 'gfirem_signature', $base_url . 'js/signature.js', array( "jquery" ), $this->version, true );
+			$params          = array();
+			$signatureFields = FrmField::get_all_types_in_form( $this->form_id, $this->slug );
+			foreach ( $signatureFields as $key => $value ) {
+				$backgroundcolor                                  = FrmField::get_option( $value, "backgroundcolor" );
+				$pencolor                                         = FrmField::get_option( $value, "pencolor" );
+				$width                                            = FrmField::get_option( $value, "width" );
+				$height                                           = FrmField::get_option( $value, "height" );
+				$params['config'][ 'field_' . $value->field_key ] = array(
+					'background' => $backgroundcolor,
+					'pencolor'   => $pencolor,
+					'width'      => $width,
+					'height'     => $height
+				);
+			}
+			wp_localize_script( 'gfirem_signature', 'gfirem_signature', $params );
+		}
 	}
 	
 	/**
@@ -223,10 +229,10 @@ class signature extends gfirem_field_base {
 		if ( empty( $replace_with ) ) {
 			return $replace_with;
 		}
-		$field       = (array) $field;
-		$html_id     = $field['field_key'];
-		$print_value = $replace_with;
-		$field_name  = 'item_meta[' . $field['id'] . ']';
+		$field          = (array) $field;
+		$html_id        = $field['field_key'];
+		$print_value    = $replace_with;
+		$field_name     = 'item_meta[' . $field['id'] . ']';
 		$this->is_front = true;
 		ob_start();
 		include dirname( __FILE__ ) . '/view/field_signature.php';
