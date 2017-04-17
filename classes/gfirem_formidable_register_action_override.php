d6262fc79148a29b33ee558d25abdfa556249c20
gfirem@gmail.com <gfirem@gmail.com>
2017-04-17 15:22:32 -0400

    applying freemius plan to the fields

--- classes/gfirem_formidable_register_action_override.php
+++ classes/gfirem_formidable_register_action_override.php
@@ -1,42 +1,40 @@
 <?php
 /**
- * @package WordPress
+ * @package    WordPress
  * @subpackage Formidable, gfirem
- * @author GFireM
- * @copyright 2017
- * @link http://www.gfirem.com
- * @license http://www.apache.org/licenses/
+ * @author     GFireM
+ * @copyright  2017
+ * @link       http://www.gfirem.com
+ * @license    http://www.apache.org/licenses/
  *
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-if ( class_exists( "FrmRegAction" ) ):
-	
-	class gfirem_formidable_register_action_override extends FrmRegAction {
-		
-		function __construct() {
-			parent::__construct();
-		}
-		
-		function form( $form_action, $args = array() ) {
-			$form = isset( $args['form'] ) ? $args['form'] : false;
-			
-			if ( ! $form ) {
-				return;
+if ( gfirem_fs::getFreemius()->is_plan__premium_only( gfirem_fs::$professional ) ) {
+	if ( class_exists( "FrmRegAction" ) ) {
+		class gfirem_formidable_register_action_override extends FrmRegAction {
+			function __construct() {
+				parent::__construct();
 			}
 			
-			global $wpdb;
-			
-			$fields = FrmField::getAll( $wpdb->prepare( 'fi.form_id=%d', $form->id ) . " and fi.type not in ('end_divider', 'divider', 'html', 'break', 'captcha', 'rte')", ' ORDER BY field_order' );
-			
-			$show_auto_login = FrmRegActionHelper::is_auto_login_visible( $form_action );
-			
-			$path_to_template = gfirem_manager::load_field_template( '_register_settings' );
-			include "$path_to_template";
+			function form( $form_action, $args = array() ) {
+				$form = isset( $args['form'] ) ? $args['form'] : false;
+				
+				if ( ! $form ) {
+					return;
+				}
+				
+				global $wpdb;
+				
+				$fields = FrmField::getAll( $wpdb->prepare( 'fi.form_id=%d', $form->id ) . " and fi.type not in ('end_divider', 'divider', 'html', 'break', 'captcha', 'rte')", ' ORDER BY field_order' );
+				
+				$show_auto_login = FrmRegActionHelper::is_auto_login_visible( $form_action );
+				
+				$path_to_template = gfirem_manager::load_field_template( '_register_settings' );
+				include "$path_to_template";
+			}
 		}
 	}
-
-endif;
+}
