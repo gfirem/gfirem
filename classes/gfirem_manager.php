14ef2c15a65b324c69b772e869befa0b095ff3e7
gfirem@gmail.com <gfirem@gmail.com>
2017-05-16 22:28:19 -0300

    disabling the integration with registration action

--- classes/gfirem_manager.php
+++ classes/gfirem_manager.php
@@ -76,12 +76,12 @@ class gfirem_manager {
 				}
 				if ( gfirem_fs::getFreemius()->is_plan__premium_only( gfirem_fs::$professional ) ) {
 					//Override the register action from formidable
-					if ( self::is_formidable_registration_active() ) {
-						require_once 'gfirem_formidable_register_action_override.php';
-						if ( class_exists( 'gfirem_formidable_register_action_override' ) && class_exists( 'FrmRegAction' ) ) {
-							add_action( 'frm_registered_form_actions', array( $this, 'override_register_actions' ), 9999 );
-						}
-					}
+//					if ( self::is_formidable_registration_active() ) {
+//						require_once 'gfirem_formidable_register_action_override.php';
+//						if ( class_exists( 'gfirem_formidable_register_action_override' ) && class_exists( 'FrmRegAction' ) ) {
+//							add_action( 'frm_registered_form_actions', array( $this, 'override_register_actions' ), 9999 );
+//						}
+//					}
 				}
 				
 				//load all teh fields
