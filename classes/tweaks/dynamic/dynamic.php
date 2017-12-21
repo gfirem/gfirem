33bce4e9d5918236ff15d42bec62610775655829
Guillermo Figueroa Mesa <gfirem@gmail.com>
2017-12-20 22:38:09 -0300

    * Fixing the Dynamic/Lookup Field Tweaks.

--- classes/tweaks/dynamic/dynamic.php
+++ classes/tweaks/dynamic/dynamic.php
@@ -10,7 +10,7 @@
  *
  */
 class dynamic extends gfirem_field_base {
-	
+
 	public function __construct() {
 		parent::__construct( 'dynamic', __( 'Dynamic Field', 'gfirem-locale' ),
 			array(
@@ -28,25 +28,24 @@ class dynamic extends gfirem_field_base {
 			     ( ! empty( $field_global_options[ 'enabled_' . $this->slug . '_unique_filter' ] ) && $field_global_options[ 'enabled_' . $this->slug . '_unique_filter' ] == '1' )
 			) {
 				add_filter( 'frm_setup_new_fields_vars', array( $this, 'filter_dynamic_options__premium_only' ), 25, 2 );
-				
+
 				add_action( 'frm_field_options_form', array( $this, 'field_option_form__premium_only' ), 10, 3 );
 				add_action( 'frm_update_field_options', array( $this, 'field_options__premium_only' ), 10, 3 );
 			}
 		}
-//		$this->slug = 'data';//HAck to include the options inside the existing fields
 	}
-	
+
 	public function field_option_form__premium_only( $field, $display, $values ) {
 		if ( $field['type'] != 'data' && $field['type'] != 'lookup' ) {
 			return;
 		}
-		
+
 		foreach ( $this->defaults as $k => $v ) {
 			if ( ! isset( $field[ $k ] ) ) {
 				$field[ $k ] = $v;
 			}
 		}
-		
+
 		$fields_for_filter = array();
 		if ( ! empty( $field['form_select'] ) ) {
 			$form_target       = FrmField::get_type( $field['form_select'], 'form_id' );
@@ -59,23 +58,23 @@ class dynamic extends gfirem_field_base {
 		if ( $field['dynamic_field_unique_filter'] == "1" ) {
 			$show_filter_unique = "checked='checked'";
 		}
-		
+
 		include dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . 'dynamic_options.php';
 	}
-	
+
 	public function field_options__premium_only( $field_options, $field, $values ) {
 		if ( $field->type != 'data' && $field->type != 'lookup' ) {
 			return $field_options;
 		}
-		
+
 		foreach ( $this->defaults as $opt => $default ) {
 			$field_options[ $opt ] = isset( $values['field_options'][ $opt . '_' . $field->id ] ) ? $values['field_options'][ $opt . '_' . $field->id ] : $default;
 		}
-		
+
 		return $field_options;
 	}
-	
-	
+
+
 	public function filter_dynamic_options__premium_only( $values, $field ) {
 		$field_global_options = get_option( $this->slug );
 		if ( ! empty( $field_global_options[ 'enabled_' . $this->slug . '_filter_entries' ] ) && $field_global_options[ 'enabled_' . $this->slug . '_filter_entries' ] == '1' ) {
@@ -95,14 +94,25 @@ class dynamic extends gfirem_field_base {
 					if ( $field->type == 'lookup' ) {
 						$temp                                    = $field;
 						$temp->field_options['get_values_field'] = $dynamic_field_target;
-						$field2_opts                             = FrmProLookupFieldsController::get_lookup_field_values_for_conditional_logic( $temp );
+						$field2_opts                             = $this->get_independent_lookup_field_values( $dynamic_field_target, $values );
 					}
-					foreach ( $values['options'] as $id => $v ) {
-						$content = $this->replace_shortcode( null, $dynamic_field_target_value );
-						if ( isset( $field2_opts[ $id ] ) && ( $v == '' || $field2_opts[ $id ] == $content ) ) {//Only include values where filtering field equals Yes
-							continue;
+					if ( ! empty( $field2_opts ) ) {
+						$position = 0;
+						foreach ( $values['options'] as $id => $v ) {
+							if($field->type === 'data') {
+								$position = $id;
+							}
+							if ( ! empty( $v ) ) {
+								$content = $this->replace_shortcode( null, $dynamic_field_target_value );
+								if ( isset( $field2_opts[ $position ] ) && $field2_opts[ $position ] == $content ) {//Only include values where filtering field equals Yes
+									continue;
+								}
+								if($field->type === 'lookup') {
+									$position ++;
+								}
+								unset( $values['options'][ $position ] );
+							}
 						}
-						unset( $values['options'][ $id ] );
 					}
 					unset( $temp );
 				}
@@ -114,30 +124,153 @@ class dynamic extends gfirem_field_base {
 				$values['options'] = array_unique( $values['options'] );
 			}
 		}
-		
+
 		return $values;
 	}
-	
+
+	/**
+	 * Get the meta values for an independent lookup field
+	 *
+	 * @since 2.01.0
+	 *
+	 * @param int $linked_field_id
+	 * @param array $values
+	 *
+	 * @return array $options
+	 */
+	private function get_independent_lookup_field_values( $linked_field_id, $values ) {
+		$linked_field = FrmField::getOne( $linked_field_id );
+		if ( ! $linked_field ) {
+			return array();
+		}
+
+		$args = array();
+
+		if ( $this->need_to_filter_values_for_current_user( $values['id'], $values ) ) {
+			$current_user = get_current_user_id();
+
+			// If user isn't logged in, don't display any options
+			if ( $current_user === 0 ) {
+				return array();
+			}
+
+			$args['user_id'] = $current_user;
+		}
+
+		if ( FrmAppHelper::is_admin_page( 'formidable' ) ) {
+			$args['limit'] = 500;
+		}
+
+		$options = FrmProEntryMeta::get_all_metas_for_field( $linked_field, $args );
+
+		$options = $this->flatten_and_unserialize_meta_values( $options );
+//		$this->get_unique_values( $options );
+//		$this->order_values( $values['lookup_option_order'], $options );
+
+		return $options;
+	}
+
+	/**
+	 * Check if the values need to be filtered for the current user
+	 *
+	 * @return bool
+	 */
+	private function need_to_filter_values_for_current_user( $field_id, $field_options ) {
+		$is_filter_needed = FrmField::is_option_true_in_array( $field_options, 'lookup_filter_current_user' ) && ! current_user_can( 'administrator' ) && ! FrmAppHelper::is_admin();
+
+		return apply_filters( 'frm_lookup_is_current_user_filter_needed', $is_filter_needed, $field_id, $field_options );
+	}
+
+	/**
+	 * If meta values are arrays (checkboxes, repeating fields, etc), flatten the values to a single-dimensional array
+	 */
+	private function flatten_and_unserialize_meta_values( $meta_values ) {
+		$final_values = array();
+		foreach ( $meta_values as $meta_val ) {
+
+			$meta_val = maybe_unserialize( $meta_val );
+			if ( is_array( $meta_val ) ) {
+				$final_values = array_merge( $final_values, $meta_val );
+			} else {
+				$meta_val       = $this->decode_html_entities( $meta_val );
+				$final_values[] = $meta_val;
+			}
+		}
+
+		return $final_values;
+	}
+
+	/**
+	 * Only get unique values in Lookup Fields
+	 *
+	 * @since 2.01.0
+	 *
+	 * @param array $final_values
+	 */
+	private function get_unique_values( &$final_values ) {
+		$final_values = array_unique( $final_values );
+		$final_values = array_values( $final_values );
+	}
+
+	/**
+	 * Order the values in a Lookup Field
+	 *
+	 * @since 2.01.0
+	 *
+	 * @param string $order
+	 * @param array $final_values
+	 */
+	private function order_values( $order, &$final_values ) {
+		if ( ! $final_values ) {
+			return;
+		}
+
+		if ( $order == 'ascending' || $order == 'descending' ) {
+			natcasesort( $final_values );
+			if ( $order == 'descending' ) {
+				$final_values = array_reverse( $final_values );
+			}
+			$final_values = array_values( $final_values );
+		}
+
+		$final_values = apply_filters( 'frm_order_lookup_options', $final_values, $order );
+	}
+
+	/**
+	 * Decode HTML entities recursively
+	 */
+	private function decode_html_entities( $value ) {
+		if ( is_array( $value ) ) {
+			foreach ( $value as $key => $single_value ) {
+				$value[ $key ] = self::decode_html_entities( $single_value );
+			}
+		} else {
+			$value = html_entity_decode( $value );
+		}
+
+		return $value;
+	}
+
 	public function global_tab() {
 		add_settings_field( 'section_dynamic', __( 'Filter Entries', 'gfirem-locale' ), array( $this, 'filter_entries' ), 'dynamic', 'section_dynamic' );
 		add_settings_field( 'section_dynamic_unique', __( 'Unique Values', 'gfirem-locale' ), array( $this, 'unique_filter' ), 'dynamic', 'section_dynamic' );
 		add_settings_field( 'section_dynamic_admin', __( 'Admin Filter', 'gfirem-locale' ), array( $this, 'admin_filter' ), 'dynamic', 'section_dynamic' );
 	}
-	
+
 	public function filter_entries() {
 		echo '<p ' . $this->disable_class_tag( 'p', gfirem_fs::$professional ) . '>';
 		$this->get_view_for( 'enabled_' . $this->slug . '_filter_entries', 'checkbox', 'dynamic', array(), gfirem_fs::$professional );
 		_e( 'If tick this you get a new options inside the dynamic/lookup field to filter the entries.', 'gfirem-locale' );
 		echo '</p>';
 	}
-	
+
 	public function admin_filter() {
 		echo '<p ' . $this->disable_class_tag( 'p', gfirem_fs::$professional ) . '>';
 		$this->get_view_for( 'enabled_' . $this->slug . '_admin_filter', 'checkbox', 'dynamic', array(), gfirem_fs::$professional );
 		_e( 'If tick this the admin can avoid the above filter.', 'gfirem-locale' );
 		echo '</p>';
 	}
-	
+
 	public function unique_filter() {
 		echo '<p ' . $this->disable_class_tag( 'p', gfirem_fs::$professional ) . '>';
 		$this->get_view_for( 'enabled_' . $this->slug . '_unique_filter', 'checkbox', 'dynamic', array(), gfirem_fs::$professional );
--- classes/tweaks/upload/assets/js/upload_tweak.js
+++ classes/tweaks/upload/assets/js/upload_tweak.js
@@ -8,172 +8,175 @@
  *
  */
 jQuery(document).ready(function ($) {
-	/* <fs_premium_only> */
-	function load_dropzone_zoom() {
-		var uploadFields = __frmDropzone;
-		if (typeof uploadFields === 'undefined') {
-			return;
-		}
-		for (var i = 0; i < uploadFields.length; i++) {
-			startZoom(i);
-		}
-	}
+    /* <fs_premium_only> */
+    function load_dropzone_zoom() {
+        var uploadFields = __frmDropzone;
+        if (typeof uploadFields === 'undefined') {
+            return;
+        }
+        for (var i = 0; i < uploadFields.length; i++) {
+            startZoom(i);
+        }
+    }
 
-	load_dropzone_zoom();
+    load_dropzone_zoom();
 
-	jQuery(document).bind('ajaxComplete ', function (event, xhr, settings) {
-		if (settings.data !== undefined && settings.data.indexOf('frm_add_form_row') !== -1 && settings.data.indexOf('field_id') !== -1 && settings.data.indexOf('i') !== -1) {
-			var field_id = getParameterByName('field_id', '?' + settings.data);
-			var sections = jQuery('div[id="frm_field_' + field_id + '_container"]').find('input[name="item_meta[' + field_id + '][row_ids][]"]');
-			jQuery.each(sections, function (key, value) {
-				var current_section = jQuery(this);
-				var dropzone = jQuery(this).parent().find('div.frm_dropzone');
-				jQuery.each(dropzone, function (key, value) {
-					var dropzone_id = jQuery(this).attr('id');
-					var dropzone_fieldId = jQuery(this).parent().find('input[type="hidden"][name^="item_meta[' + field_id + '][' + current_section.val() + ']"]').attr('data-frmfile');
-					var dropzone_fieldName = 'item_meta[' + field_id + '][' + current_section.val() + '][' + dropzone_fieldId + ']';
-					var field_settings = get_field_configurations(dropzone_fieldId);
-					if (field_settings === false) {
-						return false;
-					}
-					execute_selector('#' + dropzone_id, dropzone_fieldName, field_settings.uploadMultiple);
-				});
-			});
-		}
-	});
+    jQuery(document).bind('ajaxComplete ', function (event, xhr, settings) {
+        if (settings.data !== undefined && settings.data.indexOf('frm_add_form_row') !== -1 && settings.data.indexOf('field_id') !== -1 && settings.data.indexOf('i') !== -1) {
+            var field_id = getParameterByName('field_id', '?' + settings.data);
+            var sections = jQuery('div[id="frm_field_' + field_id + '_container"]').find('input[name="item_meta[' + field_id + '][row_ids][]"]');
+            jQuery.each(sections, function (key, value) {
+                var current_section = jQuery(this);
+                var dropzone = jQuery(this).parent().find('div.frm_dropzone');
+                jQuery.each(dropzone, function (key, value) {
+                    var dropzone_id = jQuery(this).attr('id');
+                    var dropzone_fieldId = jQuery(this).parent().find('input[type="hidden"][name^="item_meta[' + field_id + '][' + current_section.val() + ']"]').attr('data-frmfile');
+                    var dropzone_fieldName = 'item_meta[' + field_id + '][' + current_section.val() + '][' + dropzone_fieldId + ']';
+                    var field_settings = get_field_configurations(dropzone_fieldId);
+                    if (field_settings === false) {
+                        return false;
+                    }
+                    execute_selector('#' + dropzone_id, dropzone_fieldName, field_settings.uploadMultiple);
+                });
+            });
+        }
+    });
 
-	function startZoom(i) {
-		var uploadFields = __frmDropzone;
-		var selector = '#' + uploadFields[i].htmlID + '_dropzone';
-		var fieldName = uploadFields[i].fieldName;
-		var fmr_action = getParameterByName('frm_action');
-		if (fmr_action === 'edit') {
-			if (selector.indexOf('-0_dropzone') !== -1) {
-				return false;
-			}
-		}
-		//Load zoom for existing fields
-		if (typeof uploadFields[i].mockFiles !== 'undefined') {
-			if (uploadFields[i].mockFiles.length > 0) {
-				init_zoom(fieldName, uploadFields[i].mockFiles, uploadFields[i].uploadMultiple);
-			}
-		}
-		execute_selector(selector, fieldName, uploadFields[i].uploadMultiple);
-	}
+    function startZoom(i) {
+        var uploadFields = __frmDropzone;
+        var selector = '#' + uploadFields[i].htmlID + '_dropzone';
+        var fieldName = uploadFields[i].fieldName;
+        var fmr_action = getParameterByName('frm_action');
+        if (fmr_action === 'edit') {
+            if (selector.indexOf('-0_dropzone') !== -1) {
+                return false;
+            }
+        }
+        //Load zoom for existing fields
+        if (typeof uploadFields[i].mockFiles !== 'undefined') {
+            if (uploadFields[i].mockFiles.length > 0) {
+                init_zoom(fieldName, uploadFields[i].mockFiles, uploadFields[i].uploadMultiple);
+            }
+        }
+        execute_selector(selector, fieldName, uploadFields[i].uploadMultiple);
+    }
 
-	function execute_selector(selector, fieldName, uploadMultiple) {
-		var field = jQuery(selector);
-		var form = field.closest('form');
-		var dropzone = Dropzone.forElement(selector);
-		dropzone.on('success', function (file, response) {
-			var mediaIDs = jQuery.parseJSON(response);
-			if (mediaIDs.length > 0) {
-				request_attachment_url(mediaIDs, fieldName, false, form);
-			}
-		});
-		dropzone.on('successmultiple', function (files, response) {
-			var mediaIDs = jQuery.parseJSON(response);
-			request_attachment_url(mediaIDs, fieldName, true, form);
-		});
-		dropzone.on('removedfile', function (file) {
-			if (file.accepted !== false && uploadMultiple !== true) {
-				jQuery('input[name="' + fieldName + '"]').val('');
-				jQuery.each($('.zoomContainer'), function (key, value) {
-					var style = jQuery(this).find('.zoomWindow').attr('style');
-					if (style.indexOf(file.name) !== -1) {
-						jQuery(this).remove();
-					}
-				});
-				$('input[name="' + fieldName + '"]').removeData('elevateZoom').removeData('zoomImage');
-			}
-		});
-	}
+    function execute_selector(selector, fieldName, uploadMultiple) {
+        var field = jQuery(selector);
+        var form = field.closest('form');
+        var dropzone = Dropzone.forElement(selector);
+        dropzone.on('success', function (file, response) {
+            var mediaIDs = jQuery.parseJSON(response);
+            if (mediaIDs.length > 0) {
+                request_attachment_url(mediaIDs, fieldName, false, form);
+            }
+        });
+        dropzone.on('successmultiple', function (files, response) {
+            var mediaIDs = jQuery.parseJSON(response);
+            request_attachment_url(mediaIDs, fieldName, true, form);
+        });
+        dropzone.on('removedfile', function (file) {
+            if (file.accepted !== false && uploadMultiple !== true) {
+                jQuery('input[name="' + fieldName + '"]').val('');
+                jQuery.each($('.zoomContainer'), function (key, value) {
+                    var style = jQuery(this).find('.zoomWindow').attr('style');
+                    if (style.indexOf(file.name) !== -1) {
+                        jQuery(this).remove();
+                    }
+                });
+                $('input[name="' + fieldName + '"]').removeData('elevateZoom').removeData('zoomImage');
+            }
+        });
+    }
 
-	function get_field_configurations(field_id) {
-		var uploadFields = __frmDropzone;
-		for (var i = 0; i < uploadFields.length; i++) {
-			if (field_id === uploadFields[i]['fieldID']) {
-				return uploadFields[i];
-			}
-		}
-		return false;
-	}
+    function get_field_configurations(field_id) {
+        var uploadFields = __frmDropzone;
+        for (var i = 0; i < uploadFields.length; i++) {
+            if (field_id === uploadFields[i]['fieldID']) {
+                return uploadFields[i];
+            }
+        }
+        return false;
+    }
 
-	function getHiddenUploadHTML(field, mediaID, fieldName) {
-		return '<input name="' + fieldName + '[]" type="hidden" value="' + mediaID + '" data-frmfile="' + field.fieldID + '" />';
-	}
+    function getHiddenUploadHTML(field, mediaID, fieldName) {
+        return '<input name="' + fieldName + '[]" type="hidden" value="' + mediaID + '" data-frmfile="' + field.fieldID + '" />';
+    }
 
-	function disableSubmitButton($form) {
-		$form.find('input[type="submit"], input[type="button"], button[type="submit"]').attr('disabled', 'disabled');
-	}
+    function disableSubmitButton($form) {
+        $form.find('input[type="submit"], input[type="button"], button[type="submit"]').attr('disabled', 'disabled');
+    }
 
-	function enableSubmitButton($form) {
-		$form.find('input[type="submit"], input[type="button"], button[type="submit"]').removeAttr('disabled');
-	}
+    function enableSubmitButton($form) {
+        $form.find('input[type="submit"], input[type="button"], button[type="submit"]').removeAttr('disabled');
+    }
 
-	function showSubmitLoading(object) {
-		if (!object.hasClass('frm_loading_form')) {
-			object.addClass('frm_loading_form');
-		}
-		disableSubmitButton(object);
-	}
+    function showSubmitLoading(object) {
+        if (!object.hasClass('frm_loading_form')) {
+            object.addClass('frm_loading_form');
+        }
+        disableSubmitButton(object);
+    }
 
-	function removeSubmitLoading(object, enable) {
-		object.removeClass('frm_loading_form');
-		if (enable === 'enable') {
-			enableSubmitButton(object);
-		}
-	}
+    function removeSubmitLoading(object, enable) {
+        object.removeClass('frm_loading_form');
+        if (enable === 'enable') {
+            enableSubmitButton(object);
+        }
+    }
 
-	function request_attachment_url(media_ids, fieldName, uploadMultiple, form) {
-		jQuery.ajax({
-			type: 'GET',
-			url: frm_js.ajax_url,
-			data: {
-				action: 'upload_tweak_attachment',
-				media_is: media_ids,
-				nonce: frm_js.nonce
-			},
-			beforeSend: function (xhr) {
-				showSubmitLoading(form);
-			},
-			success: function (attachments) {
-				attachments = jQuery.parseJSON(attachments);
-				init_zoom(fieldName, attachments, uploadMultiple);
-			}
-		}).always(function () {
-			removeSubmitLoading(form, 'enable');
-		});
-	}
+    function request_attachment_url(media_ids, fieldName, uploadMultiple, form) {
+        jQuery.ajax({
+            type: 'GET',
+            url: frm_js.ajax_url,
+            data: {
+                action: 'upload_tweak_attachment',
+                media_is: media_ids,
+                nonce: frm_js.nonce
+            },
+            beforeSend: function (xhr) {
+                showSubmitLoading(form);
+            },
+            success: function (attachments) {
+                attachments = jQuery.parseJSON(attachments);
+                init_zoom(fieldName, attachments, uploadMultiple);
+            }
+        }).always(function () {
+            removeSubmitLoading(form, 'enable');
+        });
+    }
 
-	function init_zoom(fieldName, mockFiles, uploadMultiple) {
-		if (uploadMultiple !== undefined && uploadMultiple === true) {
-			fieldName += '[]';
-		}
-		var imageContainer = jQuery('input[name="' + fieldName + '"]').parent();
-		imageContainer.find('.dz-details').hide();
-		jQuery.each(imageContainer.find('.dz-image>img'), function (key, value) {
-			var img_for_zoom = jQuery(this);
-			var img_title = jQuery(this).attr('alt');
-			if (img_title !== undefined) {
-				jQuery.each(mockFiles, function (key, value) {
-					if (img_title.indexOf(value['name']) !== -1) {
-						img_for_zoom.attr('data-zoom-image', value['file_url']);
-						img_for_zoom.elevateZoom({scrollZoom: true, cursor: 'pointer'});
-					}
-				});
-			}
-		});
-	}
+    function init_zoom(fieldName, mockFiles, uploadMultiple) {
+        if (uploadMultiple !== undefined && uploadMultiple === true) {
+            fieldName += '[]';
+        }
+        var imageContainer = jQuery('input[name="' + fieldName + '"]').parent();
+        jQuery.each(imageContainer.find('.dz-image>img'), function (key, value) {
+            var img_for_zoom = jQuery(this);
+            var img_title = jQuery(this).attr('alt');
+            if (img_title !== undefined) {
+                jQuery.each(mockFiles, function (key, value) {
+                    if (img_title.indexOf(value['name']) !== -1) {
+                        img_for_zoom.attr('data-zoom-image', value['file_url']);
+                        img_for_zoom.css('object-fit', 'fill');
+                        img_for_zoom.css('height', '120');
+                        img_for_zoom.css('width', '120');
+                        img_for_zoom.elevateZoom({scrollZoom: true, cursor: 'pointer'});
+                    }
+                });
+            }
+        });
+    }
 
-	function getParameterByName(name, url) {
-		if (!url) url = window.location.href;
-		name = name.replace(/[\[\]]/g, "\\$&");
-		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-			results = regex.exec(url);
-		if (!results) return null;
-		if (!results[2]) return '';
-		return decodeURIComponent(results[2].replace(/\+/g, " "));
-	}
-	/* </fs_premium_only> */
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    /* </fs_premium_only> */
 });
